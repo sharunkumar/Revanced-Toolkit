678baad59f89ea2e750b66e609d66a7529ed7949
Sharun <sharunksplus@gmail.com>
2023-10-08 18:54:06 -0400

    add keystore params

--- patch.ps1
+++ patch.ps1
@@ -18,8 +18,9 @@ param (
 
 $includesParam = ($Includes | ForEach-Object { "--include=$_" })
 $excludesParam = ($Excludes | ForEach-Object { "--exclude=$_" })
+$keystoreParam = ("--alias=alias", "--keystore-entry-password=ReVanced", "--keystore-password=ReVanced")
 
-java.exe -jar .\revanced\revanced-cli.jar patch -o ".\apk\$AppName-patched.apk" -b .\revanced\revanced-patches.jar -m .\revanced\integrations.apk --keystore .\revanced\revanced.keystore -r "$env:TEMP\Revanced" $includesParam $excludesParam ".\apk\$AppName.apk"
+java.exe -jar .\revanced\revanced-cli.jar patch -o ".\apk\$AppName-patched.apk" -b .\revanced\revanced-patches.jar -m .\revanced\integrations.apk --keystore .\revanced\revanced.keystore -r "$env:TEMP\Revanced" $keystoreParam $includesParam $excludesParam ".\apk\$AppName.apk"
 
 if ($Install) {
     adb.exe install --user $ForUser ".\apk\$AppName-patched.apk"
