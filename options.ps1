bf099d2368c7a77945b8f9739df85d8ba67e93e4
Sharun <sharunksplus@gmail.com>
2024-09-07 21:00:47 -0700

    code in logic to commit changes to options

--- options.ps1
+++ options.ps1
@@ -1,3 +1,5 @@
 java.exe -jar .\revanced\revanced-cli.jar options --overwrite .\revanced\revanced-patches.jar
 
 git --no-pager diff .\options.json
+
+git add options.json && git commit -m "chore: Update Options" && git push
