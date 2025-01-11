2731bbdd7a5990e1cd3757bb72d9be4cdc3055f1
Sharun <sharun@sharunkumar.com>
2025-01-11 11:09:13 -0800

    add support for downloading APKEditor jar in update script

--- update.ps1
+++ update.ps1
@@ -5,6 +5,7 @@ Save-PatchesInfo "revanced\patches.old.txt"
 
 Get-AssetFile "https://api.github.com/repos/revanced/revanced-cli/releases/latest" "*-all.jar" "revanced\revanced-cli.jar"
 Get-AssetFile "https://api.github.com/repos/revanced/revanced-patches/releases/latest" "*.rvp" "revanced\revanced-patches.rvp"
+Get-AssetFile "https://api.github.com/repos/REAndroid/APKEditor/releases/latest" "*.jar" "revanced\apkeditor.jar"
 
 Save-PatchesInfo "revanced\patches.new.txt"
 
